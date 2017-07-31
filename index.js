da64dcadde38395465c6d6ee648f231567a470f5
Michael Anderson <manderson@amesbury.com>
2017-07-31 14:53:00 -0500

    Made close return a Promise for compatibility with changes in Database-js.

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ class ADODB_Promise_Connection {
         this.__connection = connection;
     }
 
+    /**
+     * Runs the passed SQL as a query.
+     * 
+     * @param {string} sql 
+     * @returns {Promise<array>}
+     * @memberof ADODB_Promise_Connection
+     */
     query(sql) {
         var self = this;
         return new Promise((resolve, reject) => {
@@ -18,6 +25,14 @@ class ADODB_Promise_Connection {
         });
     }
 
+    /**
+     * Executes the passed SQL
+     * 
+     * @param {string} sql 
+     * @param {any} scalar 
+     * @returns {Promise<any>}
+     * @memberof ADODB_Promise_Connection
+     */
     execute(sql, scalar) {
         var self = this;
         return new Promise((resolve, reject) => {
@@ -31,14 +46,24 @@ class ADODB_Promise_Connection {
         });
     }
 
+    /**
+     * Does nothing, required for database-js
+     * 
+     * @returns {Promise<boolean>}
+     * @memberof ADODB_Promise_Connection
+     */
     close() {
-
+        return Promise.resolve(true);
     }
 }
 
 module.exports = {
+    /**
+     * @param {ConnectionObject}
+     * @returns {ADODB_Promise_Connection}
+     */
     open: function(connection_parameters) {
-        var connection_string = `Provider=Microsoft.Jet.OLEDB.4.0;Data Source=${connection_parameters.Database};${ connection_parameters.Parameters ? connection_parameters.Parameters : '' }`
+        var connection_string = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + connection_parameters.Database + ";" + (connection_parameters.Parameters ? connection_parameters.Parameters : '' );
         var base = ADODB.open(connection_string);
         return new ADODB_Promise_Connection(base);
     }
